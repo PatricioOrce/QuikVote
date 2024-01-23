aa1be6ff510c7329d670034bf392d937d0f5cea2
Patricio <patodotaop@hotmail.com>
2024-01-23 15:08:50 -0300

    asd

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,21 @@
+from http.client import METHOD_NOT_ALLOWED
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from manager.models import Poll, Option
 from poll_manager.settings import BASE_DIR
-# Create your views here.
+from django.views.decorators.csrf import csrf_protect
 
 def index(request):
     return render(request, 'templates/index.html')
 
+@csrf_protect
+def polls_controller(request):
+    if request.method == "GET":
+        get_polls(request)
+    else:
+        return METHOD_NOT_ALLOWED
+
+@csrf_protect
 def get_polls(request):
     polls = Poll.objects.all()
     polls_group = []
@@ -23,6 +32,11 @@ def get_polls(request):
 def get_options(request):
     opt_result = list(Option.objects.all().values)
     return JsonResponse(opt_result, safe=False)
+
+def create_poll(request):
+    return render(request, 'templates/create_poll.html')
+
+
 
 def get_options_by_pollId(request, pollId):
     poll = get_object_or_404(Poll, id=pollId)
